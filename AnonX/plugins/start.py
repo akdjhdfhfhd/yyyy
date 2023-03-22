f8abc92ca34750252d7fa70419af67e90f66c04f
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-22 17:18:20 +0300

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -1,12 +1,33 @@
+import asyncio
+import time
+
 from pyrogram import filters
-from pyrogram.enums import ChatType, ParseMode
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonX import BOT_MENTION, BOT_NAME, app 
-from AnonX.Helpers import gp_buttons, pm_buttons 
-from AnonX.Helpers.dossier import *
+from config import BANNED_USERS
+from config import OWNER_ID
+from strings import get_command, get_string
+from AnonX import Telegram, YouTube, app
+from AnonX.misc import SUDOERS, _boot_
+from AnonX.plugins.playlist import del_plist_msg
+from AnonX.plugins.sudoers import sudoers_list
+from AnonX.utils.database import (add_served_chat,
+                                       add_served_user,
+                                       get_served_chats,
+                                       get_served_users,
+                                       blacklisted_chats,
+                                       get_assistant, get_lang,
+                                       get_userss, is_on_off,
+                                       is_served_private_chat)
+from AnonX.utils.decorators.language import LanguageStart
+from AnonX.utils.formatters import get_readable_time
+from AnonX.utils.inline import (help_pannel, private_panel,
+                                     start_pannel)
+
+loop = asyncio.get_running_loop()
 
 
 @app.on_message(filters.command(["start"]) & ~filters.forwarded)
