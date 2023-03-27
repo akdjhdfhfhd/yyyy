8b6a7222fb54ae0ae1ac6d07554c960e304695e0
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-27 14:34:33 +0300

    Update sys.py

--- AnonX/utils/sys.py
+++ AnonX/utils/sys.py
@@ -11,7 +11,7 @@ async def bot_sys_stats():
     bot_uptime = int(time.time() - _boot_)
     cpu = psutil.cpu_percent(interval=0.5)
     mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
+    disk = psutil.disk_usage("").percent
     UP = f"{get_readable_time((bot_uptime))}"
     CPU = f"{cpu}%"
     RAM = f"{mem}%"
